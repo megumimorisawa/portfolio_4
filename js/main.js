e28d595e5c28e220a731cde239686f010d08b895
EC2 Default User <ec2-user@ip-172-31-29-26.ap-northeast-1.compute.internal>
2021-10-14 08:21:03 +0000

    レスポンシブ&js追加

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// $(function(){
+//     $("img").fadeIn(1000);
+// });
+
+$(window).on('load', function(){
+    $("#fv_img").show().animate({
+        width: "50%",
+        // left: "0",
+        // bottom: "0",
+        // right: "0",
+        // top: "0",
+        opacity: "1"
+    });
+});
