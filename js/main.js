0dee0f5fe3c722463b047d1b0a0f5ad7b279de7d
EC2 Default User <ec2-user@ip-172-31-29-26.ap-northeast-1.compute.internal>
2021-10-15 07:27:21 +0000

    jQuery追加

--- js/main.js
+++ js/main.js
@@ -33,16 +33,49 @@ $(function () {
     $('#faq_list_question1').click(function(){
         $('#faq_list_answer1').slideToggle(200);
     })
-})
+});
 
 $(function () {
     $('#faq_list_question2').click(function(){
         $('#faq_list_answer2').slideToggle(200);
     })
-})
+});
 
 $(function () {
     $('#faq_list_question3').click(function(){
         $('#faq_list_answer3').slideToggle(200);
     })
+});
+
+$('.blog_content_box_img').animate({zIndex:1},{
+	//1秒かけてアニメーション
+	duration:1000,
+	//stepは、アニメーションが進むたびに呼ばれる
+	step:function(now){
+		//nowに現在のz-indexの値（0から1に変化しているところ）が渡してもらえる
+		//0から1に向かって変化していくnowを利用して3回転（1080度）させてみる
+		$(this).css({transform:'rotateY(' + (now * 360) + 'deg)'});
+	},
+	//終わったら
+	complete:function(){
+		//次のために、元に戻しておく
+		$('.blog_content_box_img').css('zIndex', 0);
+	}
 })
+
+// $(function () {
+//     $(window).scroll(function () {
+//         const wHeight = $(window).height();
+//         const scrollAmount = $(window).scrollTop();
+//         $('.blog_content_box_img').each(function () {
+//             const targetPosition = $(this).offset().top;
+//             if(scrollAmount > targetPosition - wHeight + 60) {
+//                 $(this).addClass("scrollin_rotate")
+//                 // $('.blog_content_box_img').css(1500),
+//                 // $('.blog_content_box_img').css({
+//                 //   "transform": "rotateY(360deg)"
+//                 // });
+//             }
+//         });
+//     });
+// });
