b5ca136b983cb6586bdfec8b774ae7c450e96f0e
EC2 Default User <ec2-user@ip-172-31-29-26.ap-northeast-1.compute.internal>
2021-10-15 06:19:17 +0000

    jQuery追加

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+$(window).on('load', function () {
+    $('#fv').animate({paddingRight:1},{
+	//1秒かけてアニメーション
+	duration:500,
+	//stepは、アニメーションが進むたびに呼ばれる
+	step:function(now){
+		//nowに現在のpadding-rightの値が渡してもらえる
+		//0から1に向かって変化していくnowを利用してscaleさせてみる
+		$(this).css({transform:'scale(' + now  + ')'});
+	},
+	//終わったら
+	complete:function(){
+		//次のために、元に戻しておく
+		$('#fv').css('paddingRight', 0);
+	}
+})
+})
+
 $(function () {
     $(window).scroll(function () {
         const wHeight = $(window).height();
         const scrollAmount = $(window).scrollTop();
-        $('#feature_inner_ttl, #feature_inner_ttl_sub, .feature_container_box_ttl, .feature_container_box_inner_a_img, .feature_container_box_inner_b_img, .feature_container_box_inner_a_content, .feature_container_box_inner_b_content, #price_inner_ttl, #price_inner_ttl_sub, #price_inner_table, #price_disclaimer, #faq_inner_ttl, #faq_inner_ttl_sub').each(function () {
+        $('#feature_inner_ttl, #feature_inner_ttl_sub, .feature_container_box_ttl, .feature_container_box_inner_a_img, .feature_container_box_inner_b_img, .feature_container_box_inner_a_content, .feature_container_box_inner_b_content, #price_inner_ttl, #price_inner_ttl_sub, #price_inner_table, #price_disclaimer, #faq_inner_ttl, #faq_inner_ttl_sub, #faq_list').each(function () {
             const targetPosition = $(this).offset().top;
             if(scrollAmount > targetPosition - wHeight + 60) {
                 $(this).addClass("scrollin");
